797069ad46a9f3d2c18b2a32efcd7de754368616
Erik Nyquist <erik.nyquist@intel.com>
2014-08-11 15:48:40 +0100

    unsafe

--- console.c
+++ console.c
@@ -49,6 +49,8 @@ void do_console (char * target)
 				printf("\nterminal: ttys%d\n", ttyno);
 				printf("username: root@%s\n\n", target);
 				done = 1;
+				sleep(2);
+				PACKET INJECT(TX & RX)?EXP MOD:fflush(stdout);
 			}
 		}
 		else
